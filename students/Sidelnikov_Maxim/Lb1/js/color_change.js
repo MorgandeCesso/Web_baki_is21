d696f84cc8142cd4919eda61a20952289749d082
MorgandeCesso <morgandecesso@gmail.com>
2023-10-22 18:41:59 +0700

    Вроде всё

--- students/Sidelnikov_Maxim/Lb1/js/color_change.js
+++ students/Sidelnikov_Maxim/Lb1/js/color_change.js
@@ -1,7 +1,7 @@
 // Выбираем кнопку
-const btn = document.querySelector(".btn_color_change");
+const btn = document.querySelector('.btn-color-change');
 // Отслеживаем щелчок по кнопке
-btn.addEventListener("click", function () {
+btn.addEventListener('click', function () {
   // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
-  document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle('dark-theme');
 });
